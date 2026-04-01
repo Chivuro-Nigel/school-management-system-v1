780396a3c90036f03273f8b9cbeb7dbea31aaa2d
Nigel Chivuro <nigelchivuro06@gmail.com>
2026-04-01 13:12:27 +0200

    modified login

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -25,8 +25,8 @@ function Login() {
     setError("");
 
     try {
-      const response = await axios.post<LoginResponse>(
-        "http://127.0.0.1:8000/api/accounts/login/",
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/accounts/login/`,
         {
           user_id: userId,
           password: password,
